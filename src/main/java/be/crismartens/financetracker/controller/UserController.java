5157d6a753068c82f817fe80159d8cf9e220edc1
Cris-Martens <martens1cris@gmail.com>
2026-04-21 20:25:14 +0200

    initial commit

---
+++ src/main/java/be/crismartens/financetracker/controller/UserController.java
@@ -0,0 +1,10 @@
+package be.crismartens.financetracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+}
