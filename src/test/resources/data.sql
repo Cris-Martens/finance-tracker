a660287afe0294925ea72d457fa80108bafbc282
Cris-Martens <martens1cris@gmail.com>
2026-05-03 22:35:32 +0200

    database set up for repository tests

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,9 +1,37 @@
-INSERT INTO app_user(username, email, password, authorities)
-    VALUES ('Mark', 'mark@google.com', '$2a$10$xyh/VKW5IAR88h.j6f4AYe7NqnHEK6a47X8j8LyHrebGHKb20yxMG', 'ROLE_USER');
-INSERT INTO app_user(username, email, password, authorities)
-    VALUES ('JohnDoe', 'johndoe@gmail.com', '$2a$10$wfObPax/4e4eyTtHlzRVq.gYCJA/ZFV0Gj7Zz29soqm63KvbM7IAq', 'ROLE_USER');
+INSERT INTO app_user(username, email, password, authority)
+    VALUES ('Mark', 'mark@google.com', '$2a$10$xyh/VKW5IAR88h.j6f4AYe7NqnHEK6a47X8j8LyHrebGHKb20yxMG', 'ROLE_USER'),
+           ('JohnDoe', 'johndoe@gmail.com', '$2a$10$wfObPax/4e4eyTtHlzRVq.gYCJA/ZFV0Gj7Zz29soqm63KvbM7IAq', 'ROLE_USER');
 
-INSERT INTO account_info(first_name, last_name, country, monthly_income, app_user)
-    VALUES ('Mark', 'Marquez', 'Belgium', 2400, 2);
-INSERT INTO account_info(first_name, last_name, country, monthly_income, app_user)
-VALUES ('John', 'Doe', 'France', 2500, 3);
+## INSERT INTO account_info(first_name, last_name, country, monthly_income, app_user)
+##    VALUES ('Mark', 'Marquez', 'Belgium', 2400, 2),
+##           ('John', 'Doe', 'France', 2500, 3);
+
+INSERT INTO category (id, name)
+    VALUES  (1, 'Housing'),
+            (2, 'Utilities'),
+            (3, 'Groceries'),
+            (4, 'Dining out/ takeout'),
+            (5, 'Transportation'),
+            (6, 'Insurance'),
+            (7, 'Healthcare'),
+            (8, 'Personal Care'),
+            (9, 'Clothing & Shoes'),
+            (10, 'Child Care'),
+            (11, 'Pets'),
+            (12, 'Subscriptions'),
+            (13, 'Entertainment'),
+            (14, 'Travel'),
+            (15, 'Education');
+
+INSERT INTO budget (user_id, amount, category_id)
+    VALUES (1, 250.0, 2),
+           (1, 850.0, 1),
+           (1, 50.0, 5);
+
+INSERT INTO expenses (expense_date, amount, description, user_id, category_id)
+    VALUES ('2026-04-28', 690.00, 'Rent', 1, 1),
+           ('2026-04-19', 10.0, 'Metro', 1, 5),
+           ('2026-02-17', 30.0, 'Netflix', 1, 12),
+           ('2026-03-12', 70.0, 'Dentist', 1, 7),
+           ('2026-04-15', 100.0, 'Food', 1, 3),
+           ('2026-03-24', 180.00, '', 1, 2);
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,22 +1,61 @@
 DROP TABLE IF EXISTS account_info;
+DROP TABLE IF EXISTS expenses;
+DROP TABLE IF EXISTS budget;
+DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS app_user;
 
+CREATE TABLE app_user
+(
+    id              BIGINT AUTO_INCREMENT NOT NULL,
+    username        VARCHAR(255) NOT NULL,
+    email           VARCHAR(255) NOT NULL,
+    password        VARCHAR(255) NOT NULL,
+    authority       VARCHAR(255) NULL,
+    CONSTRAINT pk_app_user PRIMARY KEY (id)
+);
+
+CREATE TABLE category
+(
+    id   BIGINT       NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_category PRIMARY KEY (id)
+);
+
+CREATE TABLE expenses (
+    id              BIGINT AUTO_INCREMENT NOT NULL,
+    expense_date    DATE   NOT NULL,
+    amount          DOUBLE NOT NULL,
+    description     VARCHAR(255) NULL,
+    user_id         BIGINT NOT NULL,
+    category_id     BIGINT NOT NULL,
+    CONSTRAINT pk_expenses PRIMARY KEY (id),
+    CONSTRAINT FK_EXPENSES_ON_USER
+        FOREIGN KEY (user_id) REFERENCES app_user(id)
+            ON DELETE CASCADE
+);
+
 CREATE TABLE account_info (
-    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id              BIGINT AUTO_INCREMENT NOT NULL,
     first_name      VARCHAR(255),
     last_name       VARCHAR(255),
     country         VARCHAR(255),
     monthly_income  VARCHAR(255),
     user_id         BIGINT NOT NULL UNIQUE ,
-    CONSTRAINT pk_account_info PRIMARY KEY (id)
+    CONSTRAINT pk_account_info PRIMARY KEY (id),
+    CONSTRAINT FK_ACCOUNT_INFO_ON_USER
+        FOREIGN KEY (user_id) REFERENCES app_user (id)
+            ON DELETE CASCADE
 );
 
-CREATE TABLE app_user
+CREATE TABLE budget
 (
-    id        BIGINT AUTO_INCREMENT NOT NULL,
-    username  VARCHAR(255) NOT NULL,
-    email     VARCHAR(255) NOT NULL,
-    password  VARCHAR(255) NOT NULL,
-    authority VARCHAR(255) NULL,
-    CONSTRAINT pk_app_user PRIMARY KEY (id)
+    id            BIGINT AUTO_INCREMENT NOT NULL,
+    user_id       BIGINT NOT NULL,
+    amount        DOUBLE NOT NULL UNIQUE,
+    category_id   BIGINT NOT NULL UNIQUE,
+    CONSTRAINT PK_BUDGET PRIMARY KEY (id),
+    CONSTRAINT FK_BUDGET_ON_CATEGORY FOREIGN KEY (category_id) REFERENCES category (id),
+    CONSTRAINT FK_BUDGET_ON_USER
+        FOREIGN KEY (user_id) REFERENCES app_user (id)
+            ON DELETE CASCADE
 );
