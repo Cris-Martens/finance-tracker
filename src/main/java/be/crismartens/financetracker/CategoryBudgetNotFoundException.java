37439a828de98054c0373c4aa62593b449467e76
Cris-Martens <martens1cris@gmail.com>
2026-05-08 13:53:49 +0200

    Implement docker countainers

--- src/main/java/be/crismartens/financetracker/CategoryBudgetNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package be.crismartens.financetracker;
-
-public class CategoryBudgetNptFoundException extends RuntimeException {
-  public CategoryBudgetNptFoundException(String message) {
-    super(message);
-  }
-}
